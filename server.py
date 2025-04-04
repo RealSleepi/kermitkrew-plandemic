393c184c1e10e48d5ce07135546b1398c86acc98
RealSleepi <realyhk@protonmail.com>
2025-04-04 07:49:32 -0400

    fix links and add event-creation ui

--- server.py
+++ server.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')  # This will look for index.html in the templates folder
 
+@app.route('/create-event')
+def create_event():
+    return render_template('event-creation.html')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)  # Run the app on port 80
